07c72985db8479b5ad1fb412ac8977e082d59391
Brackie <muimikieleko@gmaio.com>
2020-07-11 13:08:57 -0700

    added classes for 3 sites

---
+++ scrapper/scrapper/spiders/sites/kenyanz.py
@@ -0,0 +1,71 @@
+from . import site
+from scrapper.items import Job
+from re import search, IGNORECASE
+
+
+class Template(site.Site):
+	
+	def __init__(self):
+		self.meta = {
+			"name": "Kenyanz.com",
+			"url": "https://www.kenyanz.com/jobs?",
+			"domain": 'https://www.kenyanz.com/',
+			"method": "GET",
+			"search_param": "q",
+			"link_selector": '.row-fluid h2.h3 a::attr(href)'
+		}
+		super().__init__(self.meta)
+
+
+	def parse(self, response):
+		job = Job()
+		job["ID"] = 1
+		job["website"]= self.meta[0]["domain"]
+		job["url"] = response.url
+		job["jobTitle"] = response.css('h1.h2::text').get()
+		job["jobType"] = response.css('selector::text').get()
+		job["positionLevel"] = response.css('selector::text').get()
+		job["positions"] = response.css('selector::text').get()
+		job["uploadDate"] = response.css('selector::text').get()
+		job["year"] = response.css('selector::text').get()
+		job["deadline"] = response.css('selector::text').get()
+		job["town"] = response.css('selector::text').get()
+		job["contact"] = response.css('selector::text').get()
+		job["readvertised"] = response.css('selector::text').get()
+		job["salary"] = response.css('selector::text').get()
+		job["company"] = response.css('selector::text').get()
+		job["technology"] = response.css('selector::text').get()
+		job["description"] = response.css('selector::text').get()
+		job["employmentType"] = response.css('selector::text').get()
+		job["skills"] = response.css('selector::text').get()
+		job["industry"] = response.css('selector::text').get()
+		job["responsibilities"] = response.css('selector::text').get()
+		job["requirements"] = response.css('selector::text').get()
+		job["country"] = "Kenya"
+		return job	
+
+	def get_description(self, divs, job):
+		divs = self.clean_page(divs)
+		text = self.clean_text(' '.join(divs))
+
+		re_list = [
+			{
+				"re": r".?KeyWord(.*?)KeyWord?",
+				"fields": [""]
+			}
+		]
+
+		self.regex_search(text, re_list, job)
+
+
+
+
+
+
+
+
+
+
+		
+
+
