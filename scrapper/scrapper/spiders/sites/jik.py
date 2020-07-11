dbee6b1eb16b12f686a064f1f4df78e003cb8290
JerryNyoike <jngugi31@gmail.com>
2020-07-11 14:04:39 +0300

    Pick title, date of posting, company, location and description from jobs in Kenya

--- scrapper/scrapper/spiders/sites/jik.py
+++ scrapper/scrapper/spiders/sites/jik.py
@@ -18,3 +18,14 @@ class Jik(site.Site):
                 "next_page_selector": "div.pagination a::attr(href)"
                 }
 
+        super().__init__(self.meta)
+
+
+    def parse(self, response):
+        job = Job()
+
+        title = response.xpath('//div[@class="job-title"]/text()').get()
+        date = response.xpath('//div[@class="job-date"]/text()').get()
+        company = response.xpath('//div[@class="job-company"]/text()').get()
+        location = response.xpath('//div[@class="job-location"]/text()').get()
+        description = ''.join(response.xpath('//div[@class="job-desc"]/text()').getall())
