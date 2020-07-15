7dc9bf1ed7e863833b27c873a78e4a4e0ffbd73e
Brackie <muimikieleko@gmail.com>
2020-07-15 13:09:42 -0700

    Add InvalidEntryPipeline

--- scrapper/scrapper/pipelines.py
+++ scrapper/scrapper/pipelines.py
@@ -8,13 +8,14 @@
 from itemadapter import ItemAdapter
 import csv
 import os
+from scrapy.exceptions import DropItem
 
 
 class CsvWriterPipeline:
     def open_spider(self, spider):
         outputFiles = 1
         for entry in os.listdir():
-            if entry.find('.csv') is not -1:
+            if entry.find('.csv') != -1:
                 outputFiles+=1
 
         self.file = open(f"KEN Webscraping Job Posts-v{outputFiles}.csv", 'w')
@@ -23,10 +24,27 @@ class CsvWriterPipeline:
         self.file.close()
 
     def process_item(self, item, spider):
-        adapter = ItemAdapter(item)
-        columns = adapter.field_names()
-        writer = csv.DictWriter(self.file, fieldnames=columns)
+        if item:
+            adapter = ItemAdapter(item)
+            columns = adapter.field_names()
+            writer = csv.DictWriter(self.file, fieldnames=columns)
 
-        if self.file.tell() == 0:
-            writer.writeheader()
-        writer.writerow(adapter.asdict())
+            if self.file.tell() == 0:
+                writer.writeheader()
+            writer.writerow(adapter.asdict())
+
+
+class InvalidEntryPipeline:
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        values = adapter.asdict()
+        empty = lambda field : field is None
+        empty_fields = filter(empty, list(values.values()))
+        
+        print("\n\n*****************")
+        print(len(list(empty_fields)))
+        print("*****************\n\n")
+        if len(list(empty_fields)) > 15:
+            raise DropItem("Too many empty values found.")
+
+        return item
