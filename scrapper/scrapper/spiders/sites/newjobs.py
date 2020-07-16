da2fc62f4263ae5eca4fd8c5237cd5737cf080da
Brackie <muimikieleko@gmail.com>
2020-07-16 00:00:59 -0700

    add site newjobs

--- scrapper/scrapper/spiders/sites/newjobs.py
+++ scrapper/scrapper/spiders/sites/newjobs.py
@@ -8,52 +8,70 @@ class NewJobs(site.Site):
 	def __init__(self):
 		self.meta = {
 			"name": "New Jobs",
-			"url": "https://www.kenyanz.com/jobs?",
-			"domain": 'https://www.kenyanz.com/',
+			"base_url": "https://newjobskenya.com/?",
+			"domain": 'https://newjobskenya.com',
 			"method": "GET",
-			"search_param": "q",
-			"link_selector": '.row-fluid h2.h3 a::attr(href)'
+			"search_param": "query",
+			"get_args": {"category": ""},
+			"link_selector": '.wpjb-column-title a::attr(href)'
 		}
+		self.search_words = [
+			{
+				"fields": ["description"],
+				"titles": "Description, Summary, Details, Opportunity, The Role, Overview"
+			},
+			{
+				"fields": ["requirements", "skills"],
+				"titles": "Qualifications, Candidate Profile, Nice To Have, Competencies, Skills, Experience, Requirements"
+			},
+			{
+				"fields": ["responsibilities"],
+				"titles": "Responsibilities, Role Purpose, Tasks, Duties"
+			},
+			{
+				"fields": ["salary"],
+				"titles": "Salary"
+			}
+		]
 		super().__init__(self.meta)
 
 
 	def parse(self, response):
 		job = Job()
 		job["ID"] = 1
-		job["website"]= self.meta[0]["domain"]
+		job["website"]= self.meta["domain"]
 		job["url"] = response.url
-		job["jobTitle"] = response.css('h1.h2::text').get()
-		job["jobType"] = response.css('selector::text').get()
-		job["positionLevel"] = response.css('selector::text').get()
-		job["positions"] = response.css('selector::text').get()
-		job["uploadDate"] = response.css('selector::text').get()
-		job["year"] = response.css('selector::text').get()
-		job["deadline"] = response.css('selector::text').get()
-		job["town"] = response.css('selector::text').get()
-		job["contact"] = response.css('selector::text').get()
-		job["readvertised"] = response.css('selector::text').get()
-		job["salary"] = response.css('selector::text').get()
-		job["company"] = response.css('selector::text').get()
-		job["technology"] = response.css('selector::text').get()
-		job["description"] = response.css('selector::text').get()
-		job["employmentType"] = response.css('selector::text').get()
-		job["skills"] = response.css('selector::text').get()
-		job["industry"] = response.css('selector::text').get()
-		job["responsibilities"] = response.css('selector::text').get()
-		job["requirements"] = response.css('selector::text').get()
+		job["jobTitle"] = response.xpath('//h1[@class="entry-title"]/text()').get()
+		job["jobType"] = response.xpath('//span[@itemprop="employmentType"]/text()').get()
+		job["positionLevel"] = response.xpath('//h1[@class="entry-title"]/text()').get()
+		job["positions"] = 1
+		job["uploadDate"] = response.xpath('//span[@class="updated"]/text()').get()
+		job["year"] = response.xpath('//span[@class="updated"]/text()').get().split(',')[-1].strip()
+		job["deadline"] = "N/A"
+		job["town"] = response.xpath('//span[@itemprop="address"]/text()').get()
+		job["reavertised"] = "N/A"
+		job["company"] = response.css('.wpjb-job-company::text').get()
+		job["technology"] = response.xpath('//span[@itemprop="occupationalCategory"]/text()').get()
+		job["employmentType"] = response.xpath('//span[@itemprop="employmentType"]/text()').get()
+		job["industry"] = response.xpath('//span[@itemprop="occupationalCategory"]/text()').get()
 		job["country"] = "Kenya"
+		titles = response.xpath('//h3/text() | //strong /text() | //bold/text()').getall()
+		divs = response.css('.wpjb-job-content *::text').getall()
 		return job	
 
-	def get_description(self, divs, job):
+	def get_description(self, titles, divs, job):
 		divs = self.clean_page(divs)
+		titles = self.clean_page(titles)
 		text = self.clean_text(' '.join(divs))
 
 		re_list = [
 			{
-				"re": r".?KeyWord(.*?)KeyWord?",
-				"fields": [""]
+				"re": r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}",
+				"fields": ["contact"]
 			}
 		]
+
+		self.get_search_words(titles, re_list)
 
 		self.regex_search(text, re_list, job)
 
