5800fed2e5c12d2450eed6cd4ccdd02682db08c8
JerryNyoike <jngugi31@gmail.com>
2020-07-18 15:30:32 +0300

    Urlencode spaces as %20 to avoid redirection

--- scrapper/scrapper/spiders/sites/site.py
+++ scrapper/scrapper/spiders/sites/site.py
@@ -1,6 +1,6 @@
 from logging import info
 from re import search, sub, IGNORECASE
-from urllib.parse import urlencode
+from urllib.parse import urlencode, quote
 
 class Site(object):
 
@@ -18,7 +18,7 @@ class Site(object):
 		'''
 		createQueryPairs = lambda keyword : {identifier: keyword}
 
-		makeUrl = lambda keywordPair : baseUrl+urlencode(keywordPair)
+		makeUrl = lambda keywordPair : baseUrl+urlencode(keywordPair, quote_via=quote)
 
 		params = list(map(createQueryPairs, searchWords))
 
