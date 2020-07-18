7b7287dd4ac417845d67d602c02a9dcf68ca7a28
Brackie <muimikieleko@gmail.com>
2020-07-18 13:56:12 +0300

    Registered new sites emploi, dailyjobsik

--- scrapper/scrapper/spiders/__init__.py
+++ scrapper/scrapper/spiders/__init__.py
@@ -132,13 +132,15 @@ class JobSpider(sc.Spider):
     def get_sites(self):
         '''This function returns a list of site objects representing sites to be scrapped'''
         return [
-            # sites.brightermonday.BrighterMonday(),
-            sites.jiji.Jiji()
-            # sites.bestjobs.BestJobs(),
-            # sites.coopstaffing.CoopStaffing(),
-            # sites.jik.Jik(),
-            # sites.r4kenya.R4kenya(),
-            # sites.careerjet.CareerJet(),
-            # sites.careerpoint.CareerPoint(),
-            # sites.newjobs.NewJobs()
+            sites.brightermonday.BrighterMonday(),
+            sites.jiji.Jiji(),
+            sites.bestjobs.BestJobs(),
+            sites.coopstaffing.CoopStaffing(),
+            sites.jik.Jik(),
+            sites.r4kenya.R4kenya(),
+            sites.careerjet.CareerJet(),
+            sites.careerpoint.CareerPoint(),
+            sites.newjobs.NewJobs(),
+            sites.dailyjobsik.DailyJobsIK(),
+            sites.emploi.Emploi()
         ]
